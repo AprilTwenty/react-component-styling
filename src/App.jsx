fbc777504902b09be7298004d2be27a80557cd4a
April Twenty <atcahriya.nna@gmail.com>
2024-12-03 17:30:18 +0700

    add alert component and render 4 type

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,48 @@
 import "./App.css";
 import Button from "./components/Button.jsx"
+import Alert from "./components/Alert.jsx"
 
+const buttonBackgroundColor = ["#074EE8", "#07A4E8"];
+const alertProp = [
+  {
+    text: "This is error alert box",
+    icon: "frown",
+    color: "#F9C8C8"
+  },
+  {
+    text: "This is warning alert box",
+    icon: "alertTriangle",
+    color: "#F9D9C8"
+  },
+  {
+    text: "This is info alert box",
+    icon: "alertCircle",
+    color: "#F9EBC8"
+  },
+  {
+    text: "This is success alert box",
+    icon: "checkCircle",
+    color: "#CEF7CD"
+  },
+];
 
 function App() {
+  
   return (
     <div className="App">
       <div className="button-components-section">
-        {/* Render ตัว Button 2 แบบ */
-        <Button BackgroundColor="#074EE8" />
+        {//Render Button 2แบบ
+        buttonBackgroundColor.map((item, index) => (
+          <Button key={index} BackgroundColor={item} />
+        ))
         }
       </div>
       <hr />
       <div className="alert-components-section">
-        {/* Render ตัว Alert 4 แบบ */
-        <Button BackgroundColor="#07A4E8" />
+        {//Render Alert 4แบบ
+        alertProp.map((item, index) => (
+          <Alert key={index} propsColor={item.color} propsText={item.text} propsIcon={item.icon} />
+        ))
         }
       </div>
     </div>
--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,53 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */ 
+import { css } from "@emotion/react";
+import frownIcon from "./images/frown.svg"
+import alertTriangleIcon from "./images/alert-triangle.svg"
+import alertCircleIcon from "./images/alert-circle.svg"
+import checkCircleIcon from "./images/check-circle.svg"
 
-function Alert() {}
+const icons = {
+    frown: frownIcon,
+    alertTriangle: alertTriangleIcon,
+    alertCircle: alertCircleIcon,
+    checkCircle: checkCircleIcon
+}
+
+function Alert(props) {
+    const icon = icons[props.propsIcon]
+    return (
+        <div className="alert-box" css={css`
+                width: 650px;
+                height: 76px;
+                gap: 0px;
+                border-radius: 10px;
+                opacity: 0px;
+                background: ${props.propsColor};
+                display: flex;
+                align-items: center;
+                padding-left: 20px;
+            `}>
+                <img src={icon} alt={props.propsIcon} />
+            <div className="alert-text" css={css`
+                width: 545px;
+                height: 33px;
+                top: 273px;
+                left: 177px;
+                gap: 0px;
+                opacity: 0px;
+                font-family: Kanit;
+                font-size: 20px;
+                font-weight: 700;
+                line-height: 29.9px;
+                text-align: left;
+                text-underline-position: from-font;
+                text-decoration-skip-ink: none;
+                padding-left: 20px;
+
+                `}>
+                {props.propsText}
+            </div>
+        </div>
+    )
+}
+export default Alert;
